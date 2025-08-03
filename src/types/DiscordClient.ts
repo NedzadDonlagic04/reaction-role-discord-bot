f8cd092ec8bca1c86a377e8d43986ee8a0110700
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-03 14:45:05 +0200

    Added public specifier

--- src/types/DiscordClient.ts
+++ src/types/DiscordClient.ts
@@ -2,7 +2,7 @@ import { Client, Collection } from 'discord.js';
 import type { SlashCommand } from './SlashCommand.js';
 
 class DiscordClient extends Client {
-    commands: Collection<string, SlashCommand> = new Collection();
+    public commands: Collection<string, SlashCommand> = new Collection();
 }
 
 export default DiscordClient;
