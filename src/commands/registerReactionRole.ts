75ff9263f68e9f76ccf5c92c1c8e49858acbf0cc
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-03 17:24:25 +0200

    Format + linting

--- src/commands/registerReactionRole.ts
+++ src/commands/registerReactionRole.ts
@@ -8,14 +8,17 @@ import {
     TextChannel,
 } from 'discord.js';
 
-const findMessageInChannels = async (messageId: string, channels: Collection<string, NonThreadGuildBasedChannel | null>) => {
+const findMessageInChannels = async (
+    messageId: string,
+    channels: Collection<string, NonThreadGuildBasedChannel | null>,
+) => {
     for (const [_, channel] of channels) {
         if (channel instanceof TextChannel) {
             try {
                 const message = await channel.messages.fetch(messageId);
 
                 return message;
-            } catch(error) {
+            } catch (error) {
                 // Use when debugging
                 // console.error(error);
             }
@@ -23,54 +26,58 @@ const findMessageInChannels = async (messageId: string, channels: Collection<str
     }
 
     return null;
-}
+};
 
 const registerReactionRole = {
     data: new SlashCommandBuilder()
-    .setName('register-reaction-role')
-    .setDescription(
-        'Registers a message to monitor for reactions and automatically assigns or removes roles',
-    )
-.addStringOption((option: SlashCommandStringOption) =>
-                 option
-                 .setName('message-id')
-                 .setDescription('Message to register for monitoring')
-                     .setRequired(true),
-                ),
-                execute: async (interaction: ChatInputCommandInteraction) => {
-                    const messageId = interaction.options.getString('message-id');
+        .setName('register-reaction-role')
+        .setDescription(
+            'Registers a message to monitor for reactions and automatically assigns or removes roles',
+        )
+        .addStringOption((option: SlashCommandStringOption) =>
+            option
+                .setName('message-id')
+                .setDescription('Message to register for monitoring')
+                .setRequired(true),
+        ),
+    execute: async (interaction: ChatInputCommandInteraction) => {
+        const messageId = interaction.options.getString('message-id');
 
-                    if (!interaction.guild) {
-                        return interaction.reply({
-                            content: 'This command can only be used in a server channel',
-                            flags: MessageFlags.Ephemeral,
-                        });
-                    } else if (!messageId) {
-                        return interaction.reply({
-                            content: 'Message id not valid',
-                            flags: MessageFlags.Ephemeral,
-                        });
-                    }
+        if (!interaction.guild) {
+            return interaction.reply({
+                content: 'This command can only be used in a server channel',
+                flags: MessageFlags.Ephemeral,
+            });
+        } else if (!messageId) {
+            return interaction.reply({
+                content: 'Message id not valid',
+                flags: MessageFlags.Ephemeral,
+            });
+        }
 
-                    await interaction.deferReply({ flags: MessageFlags.Ephemeral, });
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
-                    try {
-                        const channels = await interaction.guild.channels.fetch();
-                        const message = await findMessageInChannels(messageId, channels);
+        try {
+            const channels = await interaction.guild.channels.fetch();
+            const message = await findMessageInChannels(messageId, channels);
 
-                        if (!message) {
-                            throw new TypeError(`Couldn't find message with id "${messageId}"`);
-                        }
+            if (!message) {
+                throw new TypeError(
+                    `Couldn't find message with id "${messageId}"`,
+                );
+            }
 
-                        await interaction.editReply(`Registered message made by "${message.author}" with the content "${message.content}"`);
-                    } catch(error) {
-                        if (error instanceof TypeError) {
-                            await interaction.editReply(error.message);
-                        } else {
-                            await interaction.editReply('Unknown error occurred');
-                        }
-                    }
-                },
+            await interaction.editReply(
+                `Registered message made by "${message.author}" with the content "${message.content}"`,
+            );
+        } catch (error) {
+            if (error instanceof TypeError) {
+                await interaction.editReply(error.message);
+            } else {
+                await interaction.editReply('Unknown error occurred');
+            }
+        }
+    },
 };
 
 export default registerReactionRole;
