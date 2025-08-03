f4323de35af891efc12e76eb0655c52aed68c411
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-03 14:44:52 +0200

    Changed file name, refactored function

---
+++ src/utils/getAllObjsFromDir.ts
@@ -0,0 +1,27 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const getAllObjsFromDir = async<T>(dirname: string): Promise<T[]> => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const objsFolderPath = path.join(__dirname, `../${dirname}`);
+
+    const objs: T[] = [];
+
+    const objFiles = fs
+        .readdirSync(objsFolderPath)
+        .filter((file) => file.endsWith('.js') || file.endsWith('.ts'));
+
+    for (const objFile of objFiles) {
+        const absolutePathToObjFile = path.join(objsFolderPath, objFile);
+        const obj = (await import(absolutePathToObjFile)).default;
+
+        objs.push(obj);
+    }
+
+    return objs;
+};
+
+export default getAllObjsFromDir;
