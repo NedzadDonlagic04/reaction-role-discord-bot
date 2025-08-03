73e556b9133814bc2ce2afd81a3ea3a403834d8f
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2025-08-03 17:16:24 +0200

    Updated config

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -2,9 +2,8 @@ import { Events } from 'discord.js';
 import Event from './../types/Event.js';
 import DiscordClient from '../types/DiscordClient.js';
 
-const clientReady = new Event(
-    Events.ClientReady,
-    (client: DiscordClient) => console.log(`Logged in as ${client.user?.tag}`)
+const clientReady = new Event(Events.ClientReady, (client: DiscordClient) =>
+    console.log(`Logged in as ${client.user?.tag}`),
 );
 
 export default clientReady;
--- src/events/interactionCreated.ts
+++ src/events/interactionCreated.ts
@@ -2,24 +2,31 @@ import { Events } from 'discord.js';
 import Event from './../types/Event.js';
 import DiscordClient from '../types/DiscordClient.js';
 
-const interactionCreate = new Event(Events.InteractionCreate, async(interaction) => {
-    if (!interaction.isChatInputCommand()) {
-        return;
-    }
+const interactionCreate = new Event(
+    Events.InteractionCreate,
+    async (interaction) => {
+        if (!interaction.isChatInputCommand()) {
+            return;
+        }
 
-    const discordClient = interaction.client as DiscordClient;
-    const slashCommand = discordClient.commands.get(interaction.commandName);
+        const discordClient = interaction.client as DiscordClient;
+        const slashCommand = discordClient.commands.get(
+            interaction.commandName,
+        );
 
-    if (!slashCommand) {
-        console.error(`No command with the name ${interaction.commandName} found`);
-        return;
-    }
+        if (!slashCommand) {
+            console.error(
+                `No command with the name ${interaction.commandName} found`,
+            );
+            return;
+        }
 
-    try {
-        await slashCommand.execute(interaction);
-    } catch (error) {
-        console.log(error);
-    }
-});
+        try {
+            await slashCommand.execute(interaction);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+);
 
 export default interactionCreate;
